d0ed2f02b0ee0c60e780c38a01e99a0477dd1159
Tyron <tyron.burkhardt.za@gmail.com>
2016-07-07 15:00:18 +0200

    added node modules again and updated sql script

--- nelisaDB(1).sql
+++ nelisaDB(1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 05, 2016 at 08:30 AM
+-- Generation Time: Jul 07, 2016 at 02:59 PM
 -- Server version: 5.5.49-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.17
 
@@ -732,6 +732,30 @@ INSERT INTO `sales` (`id`, `date`, `product_id`, `sold`, `price`) VALUES
 (442, '2016-03-01', 2, 4, 2.00),
 (443, '2016-03-01', 1, 5, 2.00),
 (444, '2016-03-01', 12, 12, 3.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `locked` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `admin`, `locked`) VALUES
+(16, 'Nelisa1', '$2a$10$TPsrpIVMs/homeT4GPs.6.7frk66mQSjFbnH/Te4D9RkhJI3rrMFi', 1, 0),
+(19, 'Tyron', '$2a$10$Ouj9tU0js7tm.Rdk.qV0qepUZqaTj7h08TlGF2RIwFKC06oDMgmR6', 0, 0);
 
 --
 -- Constraints for dumped tables
